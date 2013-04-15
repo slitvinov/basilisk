bb18388d689635b7e3231d42db64416d52566f55
Stephane Popinet <s.popinet@gmail.com>
2013-04-14 22:48:31 -0400

    Wet/dry treatment for Saint-Venant
    Ignore-this: eb01da4086259612b44c99687507707c
    
    darcs-hash:9585c41447735e3e8677d46e138a5fb937ddcc0a

--- saint-venant.h
+++ saint-venant.h
@@ -1,7 +1,12 @@
 /* An implementation of:
- *    Kurganov, A., & Levy, D. (2002). Central-upwind schemes for the
+ *    [1] Kurganov, A., & Levy, D. (2002). Central-upwind schemes for the
  *    Saint-Venant system. Mathematical Modelling and Numerical
  *    Analysis, 36(3), 397-425.
+ * and
+ *    [2] Kurganov, A., and Guergana, P. "A second-order
+ *    well-balanced positivity preserving central-upwind scheme for
+ *    the Saint-Venant system." Communications in Mathematical
+ *    Sciences 5.1 (2007): 133-160.
  */
 
 #include "utils.h"
@@ -25,20 +30,39 @@ void (* gradient) (const scalar, vector) = generalized_minmod;
 void parameters (void);
 void init       (void);
 
-double flux (Point point, int i, double dtmax)
+static void positivity()
+{
+  /* ensure positivity of reconstruction: see section 2.2 of [2] */
+  foreach() {
+    if (w[] + gw.x[]/2. < (B[1,0] + B[])/2.)
+      gw.x[] = B[1,0] + B[] - 2.*w[]; // (2.15) of [2]
+    else if (w[] - gw.x[]/2. < (B[-1,0] + B[])/2.)
+      gw.x[] = 2.*w[] - B[-1,0] - B[]; // (2.16) of [2]
+  }
+}
+
+/* avoid division by zero */
+static double hu_over_h (double hu, double h, double epsilon)
+{
+  double h4 = sq(h)*sq(h);
+  return h*hu/sqrt((h4 + max(h4, epsilon))/2.); // (2.17) of [2]
+}
+
+static double flux (Point point, int i, double dtmax)
 {
   VARIABLES;
   double hup = hu[i,0] - ghu.x[i,0]/2., hum = hu[i-1,0] + ghu.x[i-1,0]/2.;
-  double wp  = w[i,0]  - gw.x[i,0]/2.,  wm  = w[i-1,0]  + gw.x[i-1,0]/2.;
-  double Bpm  = (B[i,0] + B[i-1,0])/2.;
+  double wp  = w[i,0] - gw.x[i,0]/2., wm = w[i-1,0] + gw.x[i-1,0]/2.;
+  double Bpm = (B[i,0] + B[i-1,0])/2.;
   double hp = wp - Bpm, hm = wm - Bpm;
-  double up = hup/hp,  um = hum/hm;
+  double epsilon = sq(delta*L0)*sq(delta*L0);
+  double up = hu_over_h (hup, hp, epsilon), um = hu_over_h (hum, hm, epsilon);
+  hup = hp*up; hum = hm*um;
   double cp = sqrt(G*hp), cm = sqrt(G*hm);
   double ap = max(up + cp, um + cm); ap = max(ap, 0.);
   double am = min(up - cp, um - cm); am = min(am, 0.);
   fw.x[i,0] = (ap*hum - am*hup + (ap*am)*(wp - wm))/(ap - am); // (4.5) of [1]
-  fhu.x[i,0] = (ap*(hum*hum/hm + sq(hm)/2.) - 
-		am*(hup*hup/hp + sq(hp)/2.) + 
+  fhu.x[i,0] = (ap*(hum*um + G*sq(hm)/2.) - am*(hup*up + G*sq(hp)/2.) + 
 		(ap*am)*(hup - hum))/(ap - am);
   double a = max(ap, -am);
   double dt = CFL*L0*delta/a;
@@ -49,12 +73,11 @@ double flux (Point point, int i, double dtmax)
 
 #define swap(a,b) { scalar tmp = a; a = b; b = tmp; }
 
-void update (scalar hu2, scalar hu1, scalar w2, scalar w1, double dt)
+static void update (scalar hu2, scalar hu1, scalar w2, scalar w1, double dt)
 {
   foreach() {
     double Bp = (B[1,0] + B[])/2., Bm = (B[-1,0] + B[])/2.;
-    double wp  = w[] + gw.x[]/2.,  wm  = w[] - gw.x[]/2.;
-    double S = G*(Bp - Bm)*(wp - Bp + wm - Bm)/2.; // (4.4) of [1]
+    double S = G*(w[] - (Bp + Bm)/2.)*(Bp - Bm); // (2.10) of [2]
     hu1[] = hu2[] + dt*(fhu.x[] - fhu.x[1,0] - S)/(L0*delta);		  
   }
   foreach()
@@ -84,7 +107,7 @@ void run (void)
 
     /* predictor */
     (* gradient) (hu, ghu); boundary (ghu.x);
-    (* gradient) (w, gw);   boundary (gw.x);
+    (* gradient) (w, gw); positivity(); boundary (gw.x);
 
     dt = DT;
     foreach()
@@ -99,7 +122,7 @@ void run (void)
 
     /* corrector */
     (* gradient) (hu, ghu); boundary (ghu.x);
-    (* gradient) (w, gw);   boundary (gw.x);
+    (* gradient) (w, gw); positivity(); boundary (gw.x);
 
     foreach()
       flux (point, 0, dt);
