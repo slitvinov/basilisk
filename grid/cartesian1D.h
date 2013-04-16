0f48f92d55a6bac152b4948fb697409616f334bb
Stephane Popinet <s.popinet@gmail.com>
2013-04-16 02:09:12 -0400

    1D Cartesian grid
    Ignore-this: 832d18377a30a799ee20e134c385b65e
    
    darcs-hash:e365b33a52f8c0addfefb6893856e44485cd0a1c

---
+++ grid/cartesian1D.h
@@ -0,0 +1,61 @@
+#define GRIDNAME "1D Cartesian grid"
+
+#include <stdlib.h>
+
+#define I     (point.i - 1)
+#define J     0
+#define DELTA (1./point.n)
+
+typedef struct {
+  char * data;
+  int i, n;
+} Point;
+
+#define data(k,l) ((double *)&point.data[(point.i + k)*datasize + (l - l)])
+
+#define foreach(clause)							\
+  OMP_PARALLEL()							\
+  Point point = *((Point *)grid);					\
+  OMP(omp for schedule(static) clause)					\
+  for (int _k = 1; _k <= point.n; _k++) {				\
+    point.i = _k;							\
+    VARIABLES
+#define end_foreach() } OMP_END_PARALLEL()
+
+#define foreach_boundary(d) 						\
+  OMP_PARALLEL()							\
+  int ig = _ig[d], jg = _jg[d];	NOT_UNUSED(ig); NOT_UNUSED(jg);		\
+  Point point = *((Point *)grid);					\
+  {									\
+    point.i = d == right ? point.n : 1;					\
+    VARIABLES
+#define end_foreach_boundary() } OMP_END_PARALLEL()
+
+#define foreach_boundary_level(d,l) foreach_boundary(d)
+#define end_foreach_boundary_level() end_foreach_boundary()
+
+#define depth() 0
+
+void init_grid (int n)
+{
+  Point * p = malloc(sizeof(Point));
+  p->n = n;
+  p->data = calloc ((n + 2), datasize);
+  grid = p;
+  init_boundaries (nvar);
+}
+
+void free_grid (void)
+{
+  Point * p = grid;
+  free (p->data);
+  free (p);
+  free_boundaries ();
+}
+
+Point locate (double x, double y)
+{
+  Point point = *((Point *)grid);
+  point.i = (x + 0.5)*point.n + GHOSTS;
+  return point;
+}
