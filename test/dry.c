ff576c42ac376e341533422c3ae26355ec104384
Stephane Popinet <s.popinet@gmail.com>
2013-04-16 22:40:23 -0400

    'Dry' lake test case
    Ignore-this: 80d7aaed5a2afcdb434782cebc9e5ada
    
    darcs-hash:df6b4300a3603fb6d39bed299709e91d83b14c32

---
+++ test/dry.c
@@ -0,0 +1,25 @@
+#include "grid/cartesian1D.h"
+#include "saint-venant1.h"
+
+void parameters()
+{
+  N = 200;
+}
+
+void init()
+{
+  hu[left] = - hu[];
+  h[right] = 0.;
+  foreach() {
+    zb[] = 0.25*(cos(pi*x/0.1) + 1.)*(fabs(x) < 0.1);
+    h[] = 0.8 - zb[];
+  }
+}
+
+event (t = {0.5, 0.75, 1, 3, 50}) {
+  foreach()
+    fprintf (stderr, "%g %g %g %g\n", x, h[], hu[], zb[]);
+  fprintf (stderr, "\n");
+}
+
+int main() { run(); }
