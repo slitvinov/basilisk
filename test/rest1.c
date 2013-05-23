1da7bbc80356a0b5feadac0ebf9c8c52b089a3b7
Stephane Popinet <s.popinet@gmail.com>
2013-05-23 03:24:10 -0400

    Lake-at-rest with an emerging island
    Ignore-this: f1abf8b1396d6376e2a503a2c7b87f4e
    
    darcs-hash:0d0dc7c6202d11f88fd1a852131c24ca3716042b

---
+++ test/rest1.c
@@ -0,0 +1,33 @@
+// lake at rest with emerged island
+#include "saint-venant1.h"
+
+void parameters()
+{
+  X0 = Y0 = -0.5;
+  N = 64;
+}
+
+int refine (Point point, void * data)
+{
+  return x < 0.1 && y < 0.1;
+}
+
+void init()
+{
+  refine_function (refine, NULL, all);
+
+  foreach() {
+    zb[] = 0.2*exp(-200*(x*x + y*y));
+    h[] = max (0.1 - zb[], 0.);
+  }
+}
+
+int event (i = 1)
+{
+  norm n = normf (u.x);
+  fprintf (stderr, "# %g %g %g\n", n.avg, n.rms, n.max);
+  foreach ()
+    printf ("%g %g %g %g %g\n", x, y, h[], zb[], u.x[]);
+}
+
+int main() { run(); }
