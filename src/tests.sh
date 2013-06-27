5fbe46e03802bba9b18e8577174dde5c597ba5e7
Stephane Popinet <s.popinet@gmail.com>
2013-06-26 22:45:41 -0400

    Makefile.tests is required in src/
    Ignore-this: 646f9ae0a5e5b2f5a360071ab04cfd6a
    
    darcs-hash:51cf050e4fb4e0568a92daa0af0656c60ae003e6

--- src/tests.sh
+++ src/tests.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
-if ! darcs show files > /dev/null 2>&1; then
-    touch Makefile.tests
-    echo "Makefile.tests not updated (not darcs-controlled)"
-else
-    echo "updating Makefile.tests"
-    (
+showfiles()
+{
+    if ! darcs show files > /dev/null 2>&1; then
+	ls *.$1 2> /dev/null | sed 's/\(.*\)/\t\1 \\/g'
+    else
+	DIR=`basename $PWD`
+	darcs show files | grep '/'$DIR'/[^/]*\.'$1'$' | \
+	    sed 's/.*\/'$DIR'\/\(.*\)/\t\1 \\/g'
+    fi
+}
+
+echo "updating Makefile.tests"
+(
     echo "# Automatically generated using 'make Makefile.tests'"
     echo "# DO NOT EDIT, edit 'Makefile' instead"
     DIR=`basename $PWD`
     echo "ALLTESTS = \\"
-    darcs show files | grep '/'$DIR'/[^/]*\.c$' | 		\
-	sed 's/.*\/'$DIR'\/\(.*\)/\t\1 \\/g'
+    showfiles c
     echo ""
     echo "PLOTS = \\"
-    darcs show files | grep '/'$DIR'/[^/]*\.plot$' | 		\
-	sed 's/.*\/'$DIR'\/\(.*\)/\t\1 \\/g'
+    showfiles plot
     echo ""
     echo "TESTS = \\"
     sed ':x; /\\$/ { N; s/\\\n//; tx }' < Makefile | 		\
@@ -31,8 +36,6 @@ else
 	sed 's/\(.*\)/\t\1 \\/g'
     echo ""
     echo "ALLPAGES = \\"
-    darcs show files | grep '/'$DIR'/[^/]*\.[ch]\.page$' |      \
-	sed 's/.*\/'$DIR'\/\(.*\)/\t\1 \\/g'
+    showfiles '[ch]\.page'
     echo ""
-    ) > Makefile.tests
-fi
+) > Makefile.tests
