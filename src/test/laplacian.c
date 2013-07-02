1e2f88fcc3275b567c32b7109a1dd48c1c54e8cd
popinet <s.popinet@gmail.com>
2013-07-01 20:20:55 -0400

    documented laplacian.c test case
    Ignore-this: f954059f193306b36cf17a3420c19840
    
    darcs-hash:09a01a2bc106023dec732e7abbe136a92fddcbc7

--- src/test/laplacian.c
+++ src/test/laplacian.c
@@ -1,3 +1,12 @@
+/**
+# Speed of elementary operations on different grids
+
+This code is compiled using either the default quadtree implementation
+or the 'multigrid' regular Cartesian grid implementation.
+
+We use a square, regular, Cartesian grid and vary the resolution from
+16^2^ to 2048^2^ (to check for the influence of memory caching). */
+
 #include "utils.h"
 
 scalar a[], b[];
@@ -9,11 +18,26 @@ int main ()
     int nloops, i;
     clock_t start, end;
 
+/**
+We fill `a` with a simple function. */
+
     foreach()
       a[] = cos(2.*pi*x)*sin(2.*pi*y);
     boundary ({a});
 
+/** 
+We set a number of loops proportional to the number of grid
+points so that times are comparable on all grids. */
+
     nloops = i = (1 << 25) >> 2*l;
+
+/**
+Here we compute
+$$
+b = \nabla^2 a
+$$
+using a 5-points Laplacian operator. */
+
     start = clock();
     while (i--)
       foreach()
@@ -21,6 +45,9 @@ int main ()
     end = clock();
     fprintf (stderr, "%d %g\n", l, 
 	     1e9*(end - start)/(double)CLOCKS_PER_SEC/(nloops*(1 << 2*l)));
+
+/**
+Something simpler: the sum of `a` over the entire mesh. */
 
     nloops = i = (1 << 25) >> 2*l;
     double sum = 0.;
@@ -31,6 +58,9 @@ int main ()
     end = clock();
     printf ("sum %d %g %g\n", l, 
 	    1e9*(end - start)/(double)CLOCKS_PER_SEC/(nloops*(1 << 2*l)), sum);
+
+/**
+And finally the restriction operator. */
 
     nloops = i = (1 << 25) >> 2*l;
     start = clock();
@@ -43,3 +73,21 @@ int main ()
     free_grid();
   }
 }
+
+/**
+## Results
+
+This graph shows the speed of the quadtree implementation for each
+operation relative to the speed on the Cartesian mesh. As expected,
+the overhead is relatively larger for the simpler operations (e.g. sum
+of all elements). This graph is quite sensitive to the exact machine
+architecture (cache hierarchy etc...).
+
+![Relative speed of simple operations on a quadtree mesh](laplacian.png)
+
+The absolute speed for the Laplacian operator on both grid
+implementations is shown below. Note that Cartesian meshes are fast!
+(i.e. hundreds of million of grid points per second).
+
+![Absolute speed of the 5-points Laplacian on Cartesian and quadtree
+ meshes](laplacian_speed.png) */
