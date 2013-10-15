999198e21fed1b885e31ef900bdfccdfaa1e21b6
popinet <popinet@basilisk.fr>
2013-10-15 12:45:56 -0400

    Documentation for the Poisson solvers
    Ignore-this: ab7a8dee1e1d084be5a1cc25d84fca7
    
    darcs-hash:3fd152f0e4172afe147f457880a81c4568613e37

--- src/poisson.h
+++ src/poisson.h
@@ -1,68 +1,168 @@
-/* Multigrid Poisson solvers */
+/**
+# Multigrid Poisson solvers
+
+We want to solve Poisson equations of the general form
+$$
+\nabla\cdot (c\nabla a) = b
+$$
+This can be done efficiently using a multigrid solver. 
+
+An important aspect of Poisson equations is that they are linear. This
+property can be used to build better estimates of a solution by
+successive *corrections* to an initial guess. If we define an
+approximate solution $\tilde{a}$ as
+$$
+\tilde{a} + da = a
+$$
+where $a$ is the exact (unknown) solution, using the linearity of the
+Laplacian we find that $da$ verifies
+$$
+\nabla\cdot(c\nabla da) = b - \nabla\cdot(c\nabla\tilde{a})
+$$
+where the right-hand-side is often called the *residual* of the
+approximate solution $\tilde{a}$. 
+
+## Homogeneous boundary conditions
+
+To close the problem, we also need to define the boundary conditions
+applicable to $da$. They will be the *homogeneous* equivalent of the
+boundary conditions applied to $\tilde{a}$. The function below applies
+the homogeneous boundary conditions of `a` to `da` for level `l` of
+the multigrid hierarchy. */
 
 static void boundary_homogeneous (scalar a, scalar da, int l)
 {
-  /* Apply homogeneous boundary conditions of a to da on all boundaries */
   for (int b = 0; b < nboundary; b++)
     foreach_boundary_level (b, l, true)
       da[ghost] = a.boundary_homogeneous[b] (point, da);
+
+/**
+On quadtree meshes, we also need to make sure that stencils are
+consistent. In the general case, this involves first restricting the
+field values from the fine mesh to the coarse mesh and then
+prolongating the field on 'halo' cells using the coarse grid
+stencils. In the case of the multigrid cycle below, the solution is
+already known on coarse grids so that the restriction operation is not
+necessary, which leaves only the prolongation operation for level
+`l`. */
+
 #if QUADTREE
-  /* we don't need to restrict because the solution is already defined
-     on coarse levels */
   halo_prolongation (l, {da});
 #endif
 }
+
+/**
+## Multigrid cycle
+
+Here we implement the multigrid cycle proper. Given an initial guess
+`a`, a residual `res`, a correction field `da` and a relaxation
+function `relax`, we will provide an improved guess at the end of the
+cycle. */
 
 void mg_cycle (scalar a, scalar res, scalar da,
 	       void (* relax) (scalar da, scalar res, int depth),
 	       int nrelax, int minlevel)
 {
+
+/**
+We first define the residual on all levels. */
+
   restriction ({res});
+
+/**
+We then proceed from the coarsest grid (`minlevel`) down to the finest grid. */
+
   for (int l = minlevel; l <= depth(); l++) {
+
+/**
+On the coarsest grid, we take zero as initial guess. */
     if (l == minlevel) {
       foreach_level_or_leaf (l)
 	da[] = 0.;
     }
-    else 
-      /* bilinear interpolation from coarser level */
+
+/**
+On all other grids, we take as initial guess the approximate solution
+on the coarser grid bilinearly interpolated onto the current grid. */
+
+    else
       foreach_level_or_leaf (l)
 	da[] = (9.*coarse(da,0,0) + 
 		3.*(coarse(da,child.x,0) + coarse(da,0,child.y)) + 
 		coarse(da,child.x,child.y))/16.;
+
+/**
+We then apply homogeneous boundary conditions and do several
+iterations of the relaxation function to refine the initial guess. */
+
     boundary_homogeneous (a, da, l);
     for (int i = 0; i < nrelax; i++) {
       relax (da, res, l);
       boundary_homogeneous (a, da, l);
     }
   }
+
+/**
+And finally we apply the resulting correction to `a`. */
+
   foreach()
     a[] += da[];
   boundary ({a});
 }
 
-// Maximum number of multigrid iterations
+/**
+## Multigrid solver
+
+The multigrid solver itself uses successive calls to the multigrid
+cycle to refine an initial guess until a specified tolerance is
+reached. 
+
+The maximum number of iterations is controlled by `NITERMAX` and the
+tolerance by `TOLERANCE` with the default values below.
+*/
+
 int NITERMAX = 100;
-// Tolerance on residual
 double TOLERANCE = 1e-3;
 
+/**
+Information about the convergence of the solver is returned in a structure. */
+
 typedef struct {
-  int i;
-  double maxres, sum;
+  int i;              // number of iterations
+  double maxres, sum; // maximum residual, sum of r.h.s.
 } mgstats;
 
+/**
+The user needs to provide a function which computes the residual field
+(and returns its maximum) as well as the relaxation function. */
+
 mgstats mg_solve (scalar a, scalar b, 
-		  double (* residual) (scalar, scalar, scalar),
+		  double (* residual) (scalar a, scalar b, scalar res),
 		  void (* relax) (scalar da, scalar res, int depth))
 {
   scalar res[], da[];
   mgstats s = {0, 0., 0.};
   foreach () // fixme: need reduction(+:s.sum)
     s.sum += b[];
+
+/**
+Here we compute the initial residual field and its maximum. */
+
   s.maxres = residual (a, b, res);
+
+/**
+We then iterates until convergence or until `NITERMAX` is reached. Note
+also that we force the solver to apply at least one cycle, even if the
+initial residual is lower than `TOLERANCE`. */
+
   for (s.i = 0; s.i < NITERMAX && (s.i < 1 || s.maxres > TOLERANCE); s.i++) {
     mg_cycle (a, res, da, relax, 4, 0);
     s.maxres = residual (a, b, res);
   }
+
+/**
+If we have reached the maximum number of iterations, we warn the user. */
+
   if (s.i == NITERMAX)
     fprintf (stderr, 
 	     "WARNING: convergence not reached after %d iterations\n"
@@ -71,13 +171,27 @@ mgstats mg_solve (scalar a, scalar b,
   return s;
 }
 
-// Poisson equation with constant coefficients
+/**
+## Poisson equation with constant coefficients
+
+We now apply the generic multigrid solver to the simplest case
+$$
+\nabla^2 a = b
+$$
+Using a standard 5-points discrete Laplacian operator, we easily get the
+corresponding relaxation function. */
 
 static void relax (scalar a, scalar b, int l)
 {
   foreach_level_or_leaf (l)
     a[] = (a[1,0] + a[-1,0] + a[0,1] + a[0,-1] - sq(Delta)*b[])/4.;
 }
+
+/**
+The equivalent residual function is also trivial to obtain in the case
+of a Cartesian grid, however the case of the quadtree mesh requires
+more careful consideration...
+ */
 
 static double residual (scalar a, scalar b, scalar res)
 {
@@ -101,12 +215,17 @@ static double residual (scalar a, scalar b, scalar res)
   return maxres;
 }
 
+/**
+Once these two functions are defined, writing the final function is trivial. */
+
 mgstats poisson (scalar a, scalar b)
 {
   return mg_solve (a, b, residual, relax);
 }
 
-// Poisson equation with variable coefficients
+/**
+## Poisson equation with variable coefficients
+*/
 
 vector alpha;
 
