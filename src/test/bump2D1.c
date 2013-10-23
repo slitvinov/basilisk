c2af69aa68e1dc591abc08a4fabafb446ae7e1b2
Stephane Popinet <popinet@basilisk.fr>
2013-10-23 05:27:51 -0400

    Documented bump2D1.c test case
    Ignore-this: 3644bf973fc4d2da96f2307856cf3cc5
    
    darcs-hash:a8dd15b4ff4241a2c9fd46850ae9d76dd8b8fd09

--- src/test/bump2D1.c
+++ src/test/bump2D1.c
@@ -1,26 +1,65 @@
-// identical to bump2D.c but using the generic solver for systems of
-// conservation laws
+/**
+# Bouncing Saint-Venant bump
+
+This test case is identical to [bump2D.c]() but using the generic
+solver for systems of conservation laws rather than the Saint-Venant
+solver. */
 
 #include "conservation.h"
+
+/**
+The only conserved scalar is the water depth `h` and the only
+conserved vector is the flow rate `q`. */
 
 scalar h[];
 vector q[];
 scalar * scalars = {h};
 vector * vectors = {q};
 
+/**
+Using these notations, the Saint-Venant system of conservation laws
+(assuming a flat topography) can be written
+$$
+\partial_t\left(\begin{array}{c}
+    h\\
+    q_x\\
+    q_y\\
+ \end{array}\right) + \nabla\cdot\left(\begin{array}{c}
+    q_x & q_y\\
+    \frac{q_x^2}{h} + \frac{Gh^2}{2} & \frac{q_xq_y}{h}\\
+    \frac{q_yq_x}{h} & \frac{q_y^2}{h} + \frac{Gh^2}{2}
+ \end{array}\right) = 0
+$$
+with $G$ the acceleration of gravity.
+
+This system is entirely defined by the `flux()` function called by the
+generic solver for conservation laws. The parameter passed to the
+function is the array `s` which contains the state variables for each
+conserved field, in the order of their definition above (i.e. scalars
+then vectors). In the function below, we first recover each value
+(`h`, `qx` and `qy`) and then compute the corresponding fluxes
+(`f[0]`, `f[1]` and `f[2]`). The minimum and maximum eigenvalues for
+the Saint-Venant system are the characteristic speeds $u \pm
+\sqrt(Gh)$. */
+
 double G = 1.;
 
 void flux (const double * s, double * f, double e[2])
 {
-  double h = s[0], q = s[1], u = q/h;
-  f[0] = q;
-  f[1] = q*u + G*h*h/2.;
-  f[2] = s[2]*u;
+  double h = s[0], qx = s[1], u = qx/h, qy = s[2];
+  f[0] = qx;
+  f[1] = qx*u + G*h*h/2.;
+  f[2] = qy*u;
   // min/max eigenvalues
   double c = sqrt(G*h);
   e[0] = u - c; // min
   e[1] = u + c; // max
 }
+
+/**
+The solver is now fully defined and we proceed with initial conditions
+etc... as when using the standard Saint-Venant solver (see
+[bump2D.c]() for details). */
 
 #define LEVEL 7
 
@@ -73,3 +112,16 @@ event adapt (i++) {
 #endif
 
 int main() { run(); }
+
+/**
+## Results
+
+The results are comparable to that of [bump2D.c](). They are not
+identical mainly because the standard Saint-Venant solver applies
+slope-limiting to velocity rather than flow rate in the present case.
+
+![Evolution of water depth with time.](bump2D1/plot.png)
+
+![Evolution of level of refinement with time.](bump2D1/level.png)
+
+*/
