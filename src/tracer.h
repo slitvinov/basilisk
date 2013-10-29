d8b33aa98804044da4d17ff0753feb30c1b61132
Stephane Popinet <popinet@basilisk.fr>
2013-10-29 12:37:51 -0400

    Documented tracer.h
    Ignore-this: d950aecd871e81706507074358980d2
    
    darcs-hash:79c2f1b589042ab28091e18e7496da85be3ce920

--- src/tracer.h
+++ src/tracer.h
@@ -1,6 +1,25 @@
+/**
+# Generic tracer advection event
+
+This event integrates advection equations of the form
+$$
+\partial_tf_i+\mathbf{u}\cdot\nabla f_i=0
+$$
+where $\mathbf{u}$ is the velocity field and $f_i$ are a list of
+passive tracers.
+
+The `tracers` list is defined elsewhere (typically by the user), the
+staggered vector field `u` and the timestep `dt` are defined by a
+solver. */
+
 extern scalar * tracers;
 extern vector u;
 extern double dt;
+
+/**
+Given these variables, and a `tracer_fluxes` function, the event
+below integrates the advection equation, using an explicit scheme with
+timestep `dt`, for each tracer in the list. */
 
 event tracer_advection (i = 1; i++)
 {
