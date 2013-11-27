07a4e1f867bcc12ad9220ccbe2a650dbe17591dd
Stephane Popinet <popinet@basilisk.fr>
2013-11-27 12:19:30 -0500

    Documentation for reversed VOF advection test case
    Ignore-this: 11f66e2a8d8f915b9286eaddcc26b617
    
    darcs-hash:3515e9a8bdd5af269b1804f20562eb8f86855cff

--- src/test/reversed.c
+++ src/test/reversed.c
@@ -1,21 +1,49 @@
-// Time-reversed advection in a vortex
+/**
+# Time-reversed VOF advection in a vortex
+
+This classical test advects and stretches an initially circular
+interface in a non-divergent vortical flows. The flow reverts in time
+and the interface should come back to its original position. The
+difference between the initial and final shapes is a measure of the
+errors accumulated during advection.
+
+We will need the advection solver combined with the VOF advection
+scheme. */
 
 #include "advection.h"
 #include "vof.h"
+
+/**
+The volume fraction is stored in scalar field `f` which is listed as
+an `interface` for the VOF solver. We do not advect any tracer with
+the default (diffusive) advection scheme of the advection solver. */
 
 scalar f[];
 scalar * interfaces = {f}, * tracers = NULL;
 int MAXLEVEL;
 
+/**
+We center the unit box on the origin and set a maximum timestep of 0.1 */
+
 void parameters() {
-  // coordinates of lower-left corner
   X0 = Y0 = -0.5;
-  // maximum timestep
   DT = .1;
 }
 
+/**
+The initial interface is a circle of radius 0.2 centered on
+(-0.2,-0.236338) (for historical reasons). We use the levelset
+function `circle()` to define this interface. 
+
+The period of the stretching cycle is set to 15, which will lead to
+strong stretching. Milder conditions can be obtained by decreasing it. */
+
 #define circle(x,y) (sq(0.2) - (sq(x + 0.2) + sq(y + .236338)))
-#define END 15.
+#define T 15.
+
+/**
+We define the levelset function $\phi$ on each vertex of the grid and
+compute the corresponding volume fraction field. */
 
 event init (i = 0) {
   scalar phi[];
@@ -26,13 +54,32 @@ event init (i = 0) {
 }
 
 event velocity (i++) {
+
+/**
+This event defines the velocity field.
+
+On quadtrees we first adapt the grid so that the estimated error on
+the volume fraction is smaller than $5\times 10^{-3}$. We limit the
+resolution at `MAXLEVEL` and we only refine the volume fraction field
+`f`. */
+
 #if QUADTREE
-  adapt_wavelet ({f}, (double[]){1e-2}, MAXLEVEL, list = {f});
+  adapt_wavelet ({f}, (double[]){5e-3}, MAXLEVEL, list = {f});
 #endif
+
+/**
+The velocity field is defined through a streamfunction $\psi$, defined
+on the vertices of the grid. */
 
   scalar psi[];
   foreach_vertex()
-    psi[] = - 1.5*sin(2.*pi*t/END)*sin((x + 0.5)*pi)*sin((y + 0.5)*pi)/pi;
+    psi[] = - 1.5*sin(2.*pi*t/T)*sin((x + 0.5)*pi)*sin((y + 0.5)*pi)/pi;
+
+/**
+We can then differentiate the streamfunction to get the velocity
+components. This guarantees that the velocity field is exactly
+non-divergent. */
+
   trash ({u});
   struct { double x, y; } f = {-1.,1.};
   foreach_face()
@@ -40,12 +87,24 @@ event velocity (i++) {
   boundary ((scalar *){u});
 }
 
-event logfile (t = {0,END}) {
+/**
+At the start and end of the simulation we check the sum, min and max
+values of the volume fraction field. The sum must be constant to
+within machine precision and the volume fraction should be bounded by
+zero and one. */
+
+event logfile (t = {0,T}) {
   stats s = statsf (f);
   fprintf (stderr, "# %f %.12f %g %g\n", t, s.sum, s.min, s.max);
 }
 
-event field (t = END) {
+/**
+To compute the error, we reinitialise field `e` at the end of the
+simulation with the initial shape and compute the difference with the
+final shape. We outputs the norms as functions of the maximum
+resolution `N`. */
+
+event field (t = T) {
   scalar phi[], e[];
   foreach_vertex()
     phi[] = circle(x,y);
@@ -57,7 +116,11 @@ event field (t = END) {
   fprintf (stderr, "%d %g %g %g\n", N, n.avg, n.rms, n.max);
 }
 
-event shape (t += END/4.) {
+/**
+We also output the shape of the reconstructed interface at regular
+intervals (but only on the finest grid considered). */
+
+event shape (t += T/4.) {
   if (N == 128)
     output_facets (f, stdout);
 }
@@ -73,9 +136,31 @@ event movie (i += 10)
 }
 #endif
 
+/**
+Finally we run the simulation for different levels of refinement. */
+
 int main() {
   for (MAXLEVEL = 5; MAXLEVEL <= 7; MAXLEVEL++) {
     N = 1 << MAXLEVEL;
     run();
   }
 }
+
+/**
+## Results
+
+We use gnuplot (see [reversed.plot]()) to compute the convergence rate
+of the error norms with and without adaptation. The convergence rates
+are comparable.
+
+![Convergence rates for constant- and adaptive grids](reversed/plot.png)
+
+The shapes of the interface at $t=0$, $t=T/4$, $t=T/2$, $t=3T/4$ and
+$t=T$ are displayed below for both sets of simulations (constant and
+adaptive), for $N=128$. The shapes for $t=T/4$ should be identical to
+those for $t=3T/4$ and similarly for $t=0$ and $t=T$ (for which we
+measure the error). Note that the errors for $t=3T/4$ seem to be much
+larger than those for $t=T$.
+
+![Shapes of the interface for $t=0$, $t=T/4$, $t=T/2$, $t=3T/4$ and
+$t=T$ for two sets of simulations](reversed/interface.png) */
