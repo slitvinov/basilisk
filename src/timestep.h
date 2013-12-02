9d836a2a5b0acd2780c95ed93e4bbe8391d6c63d
Stephane Popinet <popinet@basilisk.fr>
2013-12-02 07:25:33 -0500

    timestep() function now uses a face velocity field
    Ignore-this: 2acd7fcf98c8f5c74c61732241ab2e6f
    
    darcs-hash:1d98a45a8b253f04ddb205d8a783067c6659fb91

--- src/timestep.h
+++ src/timestep.h
@@ -1,11 +1,10 @@
-double timestep (const vector u)
+double timestep (const face vector u)
 {
   double dtmax = DT/CFL;
-  foreach(reduction(min:dtmax))
-    foreach_dimension()
-      if (u.x[] != 0.) {
-	double dt = Delta/fabs(u.x[]);
-	if (dt < dtmax) dtmax = dt;
-      }
+  foreach_face(reduction(min:dtmax))
+    if (u.x[] != 0.) {
+      double dt = Delta/fabs(u.x[]);
+      if (dt < dtmax) dtmax = dt;
+    }
   return dtmax*CFL;
 }
