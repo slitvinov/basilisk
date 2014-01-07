cf79db01e01ad33a64116fd59a626ff23c6b7173
Stephane Popinet <popinet@basilisk.fr>
2014-01-07 06:09:27 -0500

    Implicit viscosity solver takes timestep into account
    Ignore-this: 80afb902073feefdbe670db66047e875
    
    darcs-hash:fc108049e8e4119b9a01d7bddeb28a8f75060ab6

--- src/viscosity.h
+++ src/viscosity.h
@@ -4,6 +4,7 @@ struct Viscosity {
   vector u;
   face vector mu;
   scalar alpha;
+  double dt;
 };
 
 static void relax_viscosity (scalar * a, scalar * b, int l, void * data)
@@ -11,18 +12,19 @@ static void relax_viscosity (scalar * a, scalar * b, int l, void * data)
   struct Viscosity * p = data;
   (const) face vector mu = p->mu;
   (const) scalar alpha = p->alpha;
+  double dt = p->dt;
   vector u = {a[0], a[1]}, r = {b[0], b[1]};
 
   foreach_level_or_leaf (l)
     foreach_dimension()
-      u.x[] = (alpha[]*(mu.x[1,0]*u.x[1,0] + mu.x[]*u.x[-1,0] +
-			mu.y[0,1]*u.x[0,1] + mu.y[]*u.x[0,-1]
+      u.x[] = (dt*alpha[]*(mu.x[1,0]*u.x[1,0] + mu.x[]*u.x[-1,0] +
+			   mu.y[0,1]*u.x[0,1] + mu.y[]*u.x[0,-1]
 #if IMPLICIT
-			+ ((mu.x[1,0] - mu.x[])*(u.x[1,0] - u.x[-1,0]) + 
-			   (mu.y[0,1] - mu.y[])*(u.y[1,0] - u.y[-1,0]))/2.
+			   + ((mu.x[1,0] - mu.x[])*(u.x[1,0] - u.x[-1,0]) + 
+			      (mu.y[0,1] - mu.y[])*(u.y[1,0] - u.y[-1,0]))/2.
 #endif
-			) + r.x[]*sq(Delta))/
-    (sq(Delta) + alpha[]*(mu.x[1,0] + mu.x[] + mu.y[0,1] + mu.y[]));
+			   ) + r.x[]*sq(Delta))/
+    (sq(Delta) + dt*alpha[]*(mu.x[1,0] + mu.x[] + mu.y[0,1] + mu.y[]));
 
 #if TRASH
   vector u1[];
@@ -52,6 +54,7 @@ static double residual_viscosity (scalar * a, scalar * b, scalar ** presl,
   struct Viscosity * p = data;
   (const) face vector mu = p->mu;
   (const) scalar alpha = p->alpha;
+  double dt = p->dt;
   vector u = {a[0], a[1]}, r = {b[0], b[1]}, res = {resl[0], resl[1]};
   double maxres = 0.;
 #if QUADTREE
@@ -63,7 +66,7 @@ static double residual_viscosity (scalar * a, scalar * b, scalar ** presl,
       g.x[] = mu.x[]*(a[] - a[-1,0])/Delta;
     boundary_normal ({g});
     foreach (reduction(max:maxres)) {
-      res.x[] = r.x[] - u.x[] + alpha[]/Delta*
+      res.x[] = r.x[] - u.x[] + dt*alpha[]/Delta*
         (g.x[1,0] - g.x[] + g.y[0,1] - g.y[]
 #if IMPLICIT
 	 + ((mu.x[1,0] - mu.x[])*(u.x[1,0] - u.x[-1,0]) + 
@@ -79,14 +82,14 @@ static double residual_viscosity (scalar * a, scalar * b, scalar ** presl,
   foreach (reduction(max:maxres))
     foreach_dimension() {
       res.x[] = r.x[] - u.x[] +
-        alpha[]*(mu.x[1,0]*u.x[1,0] + mu.x[]*u.x[-1,0] +
-		 mu.y[0,1]*u.x[0,1] + mu.y[]*u.x[0,-1] -
-		 (mu.x[1,0] + mu.x[] + mu.y[0,1] + mu.y[])*u.x[]
+        dt*alpha[]*(mu.x[1,0]*u.x[1,0] + mu.x[]*u.x[-1,0] +
+		    mu.y[0,1]*u.x[0,1] + mu.y[]*u.x[0,-1] -
+		    (mu.x[1,0] + mu.x[] + mu.y[0,1] + mu.y[])*u.x[]
 #if IMPLICIT
-		 + ((mu.x[1,0] - mu.x[])*(u.x[1,0] - u.x[-1,0]) + 
-		    (mu.y[0,1] - mu.y[])*(u.y[1,0] - u.y[-1,0]))/2.
+		    + ((mu.x[1,0] - mu.x[])*(u.x[1,0] - u.x[-1,0]) + 
+		       (mu.y[0,1] - mu.y[])*(u.y[1,0] - u.y[-1,0]))/2.
 #endif
-		 )/sq(Delta);
+		    )/sq(Delta);
       if (fabs (res.x[]) > maxres)
 	maxres = fabs (res.x[]);
     }
@@ -99,13 +102,14 @@ mgstats viscosity (struct Viscosity p)
   vector u = p.u;
   (const) face vector mu = p.mu;
   (const) scalar alpha = p.alpha;
+  double dt = p.dt;
   vector r[];
   foreach() {
     foreach_dimension()
       r.x[] = u.x[]
 #if !IMPLICIT
-      + alpha[]*((mu.x[1,0] - mu.x[])*(u.x[1,0] - u.x[-1,0]) + 
-		 (mu.y[0,1] - mu.y[])*(u.y[1,0] - u.y[-1,0]))/(2.*sq(Delta))
+      + dt*alpha[]*((mu.x[1,0] - mu.x[])*(u.x[1,0] - u.x[-1,0]) + 
+		    (mu.y[0,1] - mu.y[])*(u.y[1,0] - u.y[-1,0]))/(2.*sq(Delta))
 #endif
       ;
   }
