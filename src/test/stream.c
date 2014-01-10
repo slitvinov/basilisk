b789c049d3018bbafcda878edaf12e32a459b714
Stephane Popinet <popinet@basilisk.fr>
2014-01-10 06:14:40 -0500

    documented stream.c
    Ignore-this: 709d2e92be28f5b70fbdabcb02a2feba
    
    darcs-hash:3b65908510b737f34af0d8a9bb16612860aa3aa9

--- src/test/stream.c
+++ src/test/stream.c
@@ -1,4 +1,17 @@
+/**
+# Merging of two vertices
+
+Studying the interaction of two incompressible vortices is interesting
+for example as a basic model of two-dimensional turbulence. Here we
+solve the incompressible 2D Euler equations using a
+vorticity--streamfunction formulation. */
+
 #include "navier-stokes/stream.h"
+
+/**
+The domain is centered on $(0,0)$ and the maximum level of refinement
+is 8 i.e. the initial grid has $N=2^8=256$ grid points per
+dimension. */
 
 #define MAXLEVEL 8
 
@@ -8,6 +21,10 @@ void parameters()
   N = 1 << MAXLEVEL;
 }
 
+/**
+The initial vorticity field is composed of two Gaussian vortices
+separated by twice *dd* and with caracteristic radii *dd/10*. */
+
 event init (i = 0)
 {
   double dd = 0.1;
@@ -16,10 +33,18 @@ event init (i = 0)
 	       exp(-(sq(x + dd) + sq(y))/(dd/10.)));
 }
 
+/**
+We output some statistics on the vorticity field and Poisson solver at
+the start and end of the simulation. */
+
 event logfile (t = {0,30}) {
   stats s = statsf (omega);
   fprintf (stderr, "%g %d %g %g %d\n", t, i, dt, s.sum, mgpsi.i);
 }
+
+/**
+We output the vorticity and level fields at regular intervals in a
+format compatible with gnuplot. */
 
 event output (t += 5) {
   static int nf = 0;
@@ -33,6 +58,10 @@ event output (t += 5) {
   nf++;
 }
 
+/**
+If we are using a quadtree grid, it is adapted using wavelet error
+control on $\omega$. */
+
 #if QUADTREE
 event adapt (i++) {
   adapt_wavelet ({omega}, (double[]){1e-2}, MAXLEVEL, list = {omega, psi});
@@ -40,3 +69,17 @@ event adapt (i++) {
 #endif
 
 int main() { run(); }
+
+/**
+## Results
+
+After running and processing by gnuplot (using [stream.plot]()) we get:
+
+![Evolution of the vorticity field with time.](vortex/plot.png)
+
+![Evolution of level of refinement with time.](vortex/level.png)
+
+## See also
+
+* [Merging of two vortices (centered Euler solver)](vortex.c)
+* [Coalescence of a pair of Gaussian vortices (Gerris logo)](http://gerris.dalembert.upmc.fr/gerris/examples/examples/logo.html) */
