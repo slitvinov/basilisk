d22fdc4dc04d33083b9d424c61b6e6fb467e2957
Jose-Maria Fullana <fullana@ida.upmc.fr>
2014-03-03 13:23:04 -0500

    Explosion test case for compressible gas dynamics
    Ignore-this: 82fa02cdcbb09cfa9844c9af4e279ef4
    
    darcs-hash:d8ef0dffacaee2002b7826f29a7e253db9117875

---
+++ src/compressible.h
@@ -0,0 +1,79 @@
+/**
+# Compressible gas dynamics
+
+The Euler system of conservation laws for a compressible gas with
+polytropic exponent $\gamma$ can be written
+
+$$
+\partial_t\left(\begin{array}{c}
+\rho\						\
+w_x\						\
+w_y\						\
+E \						\
+\end{array}\right) 
++ 
+\nabla_x \cdot\left(\begin{array}{c}
+w_x \						\
+\frac{w_x^2}{\rho} + p \			\
+\frac{w_y w_x}{\rho} \				\
+\frac{w_x}{\rho} ( E + p )
+\end{array}\right)
++ 
+\nabla_y \cdot\left(\begin{array}{c}
+w_y \						\
+\frac{w_y w_x}{\rho} \				\
+\frac{w_y^2}{\rho} + p \			\
+\frac{w_y}{\rho} ( E + p )
+\end{array}\right)
+= 0
+$$
+
+with $\rho$ the gas density, $E$ the total energy, $\mathbf{w}$ the
+gas momentum and $p$ the pressure given by
+
+$$
+p = (\gamma - 1)(E - \rho\mathbf{u}^2/2)
+$$
+
+This system can be solved using the generic solver for systems of
+conservation laws. */
+
+#include "conservation.h"
+
+/**
+The conserved scalars are the gas density $\rho$ and the total energy
+$E$. The only conserved vector is the momentum $w$.  The constant
+gamma is represented by *gammao* here, with a default value of 1.4. */
+
+scalar rho[], E[];
+vector w[];
+scalar * scalars = {rho, E};
+vector * vectors = {w};
+double gammao = 1.4 ;
+
+/**
+The system is entirely defined by the `flux()` function called by the
+generic solver for conservation laws. The parameter passed to the
+function is the array `s` which contains the state variables for each
+conserved field, in the order of their definition above (i.e. scalars
+then vectors). In the function below, we first recover each value
+($\rho$, $E$, $w_x$ and $w_y$) and then compute the corresponding
+fluxes (`f[0]`, `f[1]`, `f[2]` and `f[3]`). The minimum and maximum
+eigenvalues for the Euler system are the characteristic speeds $u \pm
+\sqrt(\gamma p / \rho)$. */
+
+void flux (const double * s, double * f, double e[2])
+{
+  double rho = s[0], E = s[1], wn = s[2], wt = s[3];
+  double un = wn/rho, p = (gammao - 1.)*(E - 0.5*(sq(wn) + sq(wt))/rho);
+
+  f[0] = wn;
+  f[1] = un*(E + p);
+
+  f[2] = un*wn + p;
+  f[3] = un*wt;
+
+  double c = sqrt(gammao*p/rho);
+  e[0] = un - c; // min
+  e[1] = un + c; // max
+}
---
+++ src/test/explosion.c
@@ -0,0 +1,106 @@
+/**
+# Two-dimensional explosion
+
+We solve the Euler equations for a compressible gas. */
+
+#include "compressible.h"
+
+/**
+We make boundary conditions free outflow. */
+
+w.y[top]    = neumann(0);
+w.y[bottom] = neumann(0);
+w.x[left]   = neumann(0);
+w.x[right]  = neumann(0);
+
+/**
+The domain spans $[-1:1]\times[-1:1]$. */
+
+#define LEVEL 7
+
+int main() {
+  X0 = Y0 = -1.;
+  L0 = 2.;
+  N = 1 << LEVEL;
+  run(); 
+}
+
+/**
+Initial conditions come from Toro's book (Riemann Solvers and
+Numerical Methods for Fluid Dynamics, 3rd Edition, Springer Ed.)
+Chapter 17 section 17.1.1 are given in terms of density ($\rho$),
+pression ($p$), velocity ($u$) both at the left and right side of the
+discontinuity placed at $R=0.4$. */
+
+event init (t = 0)
+{
+  double R = 0.4 ;
+  double rhoL = 1., rhoR = 0.125 ;
+  double VmL = 0.0, VmR = 0.0 ;
+  double pL = 1.0,  pR = 0.1 ;
+  
+/**
+Left and right initial states for $\rho$, $\mathbf{w}$ and energy
+$E = 0.5*\rho \mathbf{u}^2 + p/(\gamma-1)$. */
+  
+  foreach() {
+    double r = sqrt(sq(x) + sq(y));
+    double p;
+    if (r <= R) {
+      rho[] = rhoL;
+      w.x[] = w.y[] = VmL;
+      p = pL;
+    }
+    else {
+      rho[] = rhoR;
+      w.x[] = w.y[] = VmR;
+      p = pR;
+    }
+    E[] = 0.5*sq(w.x[])*rho[] + p/(gammao - 1.);
+    w.x[] *= x*rho[]/r;
+    w.y[] *= y*rho[]/r;
+  }
+}
+
+event print (t = 0.25)
+{
+
+/**
+At $t=0.25$ we output the values of $\rho$ and the normal velocity
+$\mathbf{u}_n$ as functions of the radial coordinate. */
+
+  foreach() {
+    double r = sqrt(sq(x) + sq(y));
+    double wn = (w.x[]*x + w.y[]*y)/r;
+    printf ("%g %g %g\n", r, rho[], wn/rho[]);
+  }
+
+/**
+For reference we also output a cross-section at $y=0$. */
+
+  for (double x = 0; x <= 1; x += 1e-2)
+    fprintf (stderr, "%g %.4f %.4f\n", x,
+	     interpolate (rho, x, 0.),
+	     interpolate (w.x, x, 0.));
+}
+
+/**
+On quadtrees, we adapt the mesh by controlling the error on the density
+field. */
+
+#if QUADTREE
+event adapt (i++) {
+  adapt_wavelet ({rho}, (double[]){1e-5}, LEVEL + 1);
+}
+#endif
+
+/**
+## Results
+
+Results are presented in terms of $\rho$ and normal velocity $u_n$ for
+Cartesian (7 levels) and adaptive (8 levels) computations. The
+numerical results compare very well with Toro's numerical experiments.
+
+![Radial density profile](explosion/plot.png)
+
+![Radial velocity profile](explosion/velocity.png) */
