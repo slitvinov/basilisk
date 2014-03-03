fbeb4a4a49b0436a790ca213e9dce6387739de8e
Stephane Popinet <popinet@basilisk.fr>
2014-03-03 14:41:04 -0500

    Documentation fixes for compressible solver
    Ignore-this: b497988bd82dfe8f8e24d234ede61815
    
    darcs-hash:947427b062e531c33df2458b97d5e9c7b29a0e9e

--- src/compressible.h
+++ src/compressible.h
@@ -1,49 +1,49 @@
 /**
 # Compressible gas dynamics
 
-The Euler system of conservation laws for a compressible gas with
-polytropic exponent $\gamma$ can be written
+The Euler system of conservation laws for a compressible gas can be
+written
 
 $$
 \partial_t\left(\begin{array}{c}
-\rho\						\
-w_x\						\
-w_y\						\
-E \						\
+\rho \\
+E    \\
+w_x  \\
+w_y  \\
 \end{array}\right) 
 + 
 \nabla_x \cdot\left(\begin{array}{c}
-w_x \						\
-\frac{w_x^2}{\rho} + p \			\
-\frac{w_y w_x}{\rho} \				\
-\frac{w_x}{\rho} ( E + p )
+w_x \\
+\frac{w_x}{\rho} ( E + p ) \\
+\frac{w_x^2}{\rho} + p \\
+\frac{w_y w_x}{\rho} \\
 \end{array}\right)
 + 
 \nabla_y \cdot\left(\begin{array}{c}
-w_y \						\
-\frac{w_y w_x}{\rho} \				\
-\frac{w_y^2}{\rho} + p \			\
-\frac{w_y}{\rho} ( E + p )
+w_y \\
+\frac{w_y}{\rho} ( E + p ) \\
+\frac{w_y w_x}{\rho} \\
+\frac{w_y^2}{\rho} + p \\
 \end{array}\right)
 = 0
 $$
 
 with $\rho$ the gas density, $E$ the total energy, $\mathbf{w}$ the
-gas momentum and $p$ the pressure given by
+gas momentum and $p$ the pressure given by the equation of state
 
 $$
 p = (\gamma - 1)(E - \rho\mathbf{u}^2/2)
 $$
 
-This system can be solved using the generic solver for systems of
-conservation laws. */
+with $\gamma$ the polytropic exponent. This system can be solved using
+the generic solver for systems of conservation laws. */
 
 #include "conservation.h"
 
 /**
 The conserved scalars are the gas density $\rho$ and the total energy
-$E$. The only conserved vector is the momentum $w$.  The constant
-gamma is represented by *gammao* here, with a default value of 1.4. */
+$E$. The only conserved vector is the momentum $\mathbf{w}$.  The constant
+$\gamma$ is represented by *gammao* here, with a default value of 1.4. */
 
 scalar rho[], E[];
 vector w[];
@@ -56,14 +56,16 @@ The system is entirely defined by the `flux()` function called by the
 generic solver for conservation laws. The parameter passed to the
 function is the array `s` which contains the state variables for each
 conserved field, in the order of their definition above (i.e. scalars
-then vectors). In the function below, we first recover each value
-($\rho$, $E$, $w_x$ and $w_y$) and then compute the corresponding
-fluxes (`f[0]`, `f[1]`, `f[2]` and `f[3]`). The minimum and maximum
-eigenvalues for the Euler system are the characteristic speeds $u \pm
-\sqrt(\gamma p / \rho)$. */
+then vectors). */
 
 void flux (const double * s, double * f, double e[2])
 {
+
+/**
+We first recover each value ($\rho$, $E$, $w_x$ and $w_y$) and then
+compute the corresponding fluxes (`f[0]`, `f[1]`, `f[2]` and
+`f[3]`). */
+
   double rho = s[0], E = s[1], wn = s[2], wt = s[3];
   double un = wn/rho, p = (gammao - 1.)*(E - 0.5*(sq(wn) + sq(wt))/rho);
 
@@ -72,6 +74,10 @@ void flux (const double * s, double * f, double e[2])
 
   f[2] = un*wn + p;
   f[3] = un*wt;
+
+/**
+The minimum and maximum eigenvalues for the Euler system are the
+characteristic speeds $u \pm \sqrt(\gamma p / \rho)$. */
 
   double c = sqrt(gammao*p/rho);
   e[0] = un - c; // min
