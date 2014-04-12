0a32b1de7e2829391ba22649be4010ef410e1a3c
Stephane Popinet <popinet@basilisk.fr>
2014-04-12 11:18:46 -0400

    Decaying turbulence example
    Ignore-this: f8efe0be345cf397ded93aa87566653
    
    darcs-hash:cbc3b7607dab5e0cc6ff7b6950f9ebd12bb10cbd

---
+++ src/examples/turbulence.c
@@ -0,0 +1,59 @@
+/**
+# Decaying two-dimensional turbulence
+
+We solve the two-dimensional incompressible Euler equations using a
+vorticity--streamfunction formulation. */
+
+#include "grid/multigrid.h"
+#include "navier-stokes/stream.h"
+
+/**
+The domain is square, of size unity and centered on the origin. The
+resolution is constant at $256^2$. */
+
+#define LEVEL 8
+
+int main() {
+  N = 1 << LEVEL;
+  X0 = Y0 = -0.5;
+  run();
+}
+
+/**
+The initial condition for vorticity is just a white noise in the range
+$[-1:1]$ .*/
+
+event init (i = 0) {
+  foreach()
+    omega[] = noise();
+}
+
+/**
+We generate images of the vorticity field every 8 timesteps up to
+$t=1000$. We fix the colorscale to $[-0.3:0.3]$. */
+
+event output (i += 8; t <= 1000) {
+  output_ppm (omega, min = -0.3, max = 0.3);
+}
+
+/**
+Finally, after completion of the simulation, we create a compressed
+GIF animation. */
+
+event gif (t = end) {
+
+  /**
+  We first convert each PPM image into GIF... */
+  
+  system ("convert out out-%04d.gif && rm -f out;"
+
+	  /**
+	  ... then use `gifsicle` to create the final compressed
+	  animated GIF. */
+	  
+	  "gifsicle --colors 256 --optimize --delay 10"
+	  " --loopcount=0 out-*.gif > omega.gif && rm -f out-*.gif");
+}
+
+/**
+![Evolution of the vorticity](turbulence/omega.gif) */
