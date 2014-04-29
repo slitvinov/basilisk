fb607a4ba477051b8002fa166b1dfd61e88b2c19
Stephane Popinet <popinet@basilisk.fr>
2014-04-29 07:24:58 -0400

    Literate python example documentation
    Ignore-this: 1d58f91d2f32bc98bac939871c677950
    
    darcs-hash:cc1a60f813cfdc2b93054a6d570a23e130f53ae3

--- src/examples/poisson.py
+++ src/examples/poisson.py
@@ -1,65 +1,55 @@
+"""
 # Poisson problem with Python
 
-In this example we access lower-level Basilisk functions from python.
+In this example we access lower-level Basilisk functions from Python.
 
 As in the [2D turbulence example](turbulence.py), we import
 matplotlib, numpy and the *stream* Basilisk module. We also use math
-functions.
+functions."""
 
-~~~python
 import matplotlib.pyplot as plt
 import numpy as np
 import stream as bas
 from math import *
-~~~
 
-We initialise the $256^2$ regular grid.
+"""
+We initialise the $256^2$ regular grid."""
 
-~~~python
 N = 256
 bas.init_grid(N)
-~~~
 
-We allocate two new scalar fields *a* and *b*.
+"""
+We allocate two new scalar fields *a* and *b*."""
 
-~~~python
 a = bas.scalar()
 b = bas.scalar()
-~~~
 
-And initialize them with simple arithmetic functions.
+"""
+And initialize them with simple arithmetic functions."""
 
-~~~python
 a.f = lambda x,y: 0.
 b.f = lambda x,y: sin(2.*pi*x)*cos(2.*pi*y)
-~~~
 
+"""
 We then call the [multigrid Poisson solver](/src/poisson.h) to solve
 $$
 \nabla^2 a = b
 $$
+"""
 
-~~~python
 bas.poisson(a,b)
-~~~
 
-And use matplolib to display the solution.
+"""
+And use matplotlib to display the solution."""
 
-~~~python
 x = np.linspace(0, 1, N)
 y = np.linspace(0, 1, N)
 X,Y = np.meshgrid(x,y)
 plt.imshow(a.f(X,Y))
 plt.show()
-~~~
 
-And free the grid.
-
-~~~python
-bas.free_grid()
-~~~
-
+"""
 Note that we could reuse the same *stream.py* module because the
 streamfunction--vorticity solver (which we don't use in this example)
 also exports the interface for the Poisson solver (which we do use in
-this example).
+this example)."""
--- src/examples/turbulence.py
+++ src/examples/turbulence.py
@@ -1,6 +1,7 @@
+"""
 # Python interface
 
-Basilisk also includes a high-level interface for the [python
+Basilisk also includes a high-level interface for the [Python
 programming
 language](http://en.wikipedia.org/wiki/Python_%28programming_language%29).
 
@@ -35,65 +36,54 @@ The next step is to create the python program itself, let's call it
 
 We first import [matplotib](http://matplotlib.org/),
 [numpy](http://www.numpy.org/) and our Basilisk module.
+"""
 
-~~~python
 import matplotlib.pyplot as plt
 import numpy as np
 import stream as bas
-~~~
 
-We define a function which sets up initial conditions. Here the
-initial vorticity field $\omega$ is just white noise.
-
-~~~python
-def init(i,t):
-    bas.omega.f = bas.noise
-~~~
-
+"""
 We setup a *numpy* regular grid which will hold the values
-interpolated from the Basilisk field.
+interpolated from the Basilisk field."""
 
-~~~python
 N = 256
 x = np.linspace(0, 1, N)
 y = np.linspace(0, 1, N)
 X,Y = np.meshgrid(x,y)
-~~~
 
+"""
 We define a function which uses *matplotlib* to generate a graph
-of the field.
+of the $\omega$ field."""
 
-~~~python
 def graph(i,t):
     print "t=",t
     Z = bas.omega.f(X,Y)
     plt.imshow(Z)
     plt.draw()
     plt.show(block=False)
-~~~
 
-We setup Basilisk's resolution to match that of our *numpy* grid.
+"""
+We setup Basilisk's resolution to match that of our *numpy* grid."""
 
-~~~python
-bas.resolution(N)
-~~~
+bas.init_grid(N)
 
-We register the initialisation function above (which happens at *i =
-0*) and the graph function which happens at *t = 0,10,20,...,1000*.
+"""
+We set initial conditions for the vorticity $\omega$: a white
+noise."""
 
-~~~python
-bas.event(init, i = 0)
+bas.omega.f = bas.noise
+
+"""
+We register the graph function which happens at *t = 0,10,20,...,1000*."""
+
 bas.event(graph, t = range(0,1000,10))
-~~~
 
-We run the simulation.
+"""
+We run the simulation."""
 
-~~~python
 bas.run()
-~~~
 
-This is just so that matplotlib does not close the window.
+"""
+This is just so that matplotlib does not close the window."""
 
-~~~python
 plt.show()
-~~~
