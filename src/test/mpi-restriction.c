260086774951622cfb1fbed70ac53d697b4c1c17
Stephane Popinet <popinet@basilisk.fr>
2014-05-04 04:23:55 -0400

    Checks for boundary conditions on levels
    Ignore-this: 8db374ed4345cc449c8ab7a37c03f7b3
    
    darcs-hash:1ded2350e86252d981b166d4065eac1228a87abe

--- src/test/mpi-restriction.c
+++ src/test/mpi-restriction.c
@@ -24,11 +24,25 @@ int main (int argc, char * argv[])
 
   output_cells (stdout);
 
+  // check boundary conditions on leaves
   foreach()
     for (int i = -1; i <= 1; i++)
       for (int j = -1; j <= 1; j++)
 	assert (s[i,j] == 1.);
 
+  // check boundary conditions on levels
+  scalar s1[];
+  for (int l = 0; l <= depth(); l++) {
+    foreach_level (l)
+      s1[] = 2;
+    boundary_level ({s1}, l);
+    foreach_level_or_leaf (l)
+      for (int i = -1; i <= 1; i++)
+	for (int j = -1; j <= 1; j++)
+	  assert (s1[i,j] == 2.);
+  }
+    
+  // check restriction 
   foreach_fine_to_coarse() {
     fprintf (stderr, "%g %g %g %d\n", x, y, s[], level);
     assert (s[] == 1.);
