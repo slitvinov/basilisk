24697083ea73c4ed796ff1afb9737f42854c60de
Stephane Popinet <popinet@basilisk.fr>
2014-07-15 09:27:19 -0400

    Cleanup of conical.c
    Ignore-this: 35d74e551f12ac80e19cf03ad838f649
    
    darcs-hash:ad06676826e87d710e2187f25ad2c37e5ded0333

--- src/test/conical.c
+++ src/test/conical.c
@@ -139,44 +139,44 @@ event outputfile (t = {9, 12, 13, 14, 20})
     l[] = level;
   output_field ({l}, stdout, N);
   nf++;
-
-  /**
-  Which gives the following wave (left column) and mesh (right column)
-  evolutions. 
-
-  ~~~gnuplot Evolution of free surface (left) and mesh (right)
-  ! awk '{ if ($1 == "file:") file = $2; else print $0 > file; }' < out
- 
-  set term @PNG enhanced size 640,1120 font ",8"
-  set output 'evolution.png'
-
-  unset key
-  unset xtics
-  unset ytics
-  
-  dry=1e-4
-  
-  set size ratio -1
-  set pm3d
-  set pm3d map
-  # set contour base
-  set palette defined ( 0 0 0 0.5647, 0.125 0 0.05882 1, 0.25 0 0.5647 1, \
-  0.375 0.05882 1 0.9333, 0.5 0.5647 1 0.4392, 0.625 1 0.9333 0,	\
-  0.75 1 0.4392 0, 0.875 0.9333 0 0, 1 0.498 0 0 )
-  
-  set multiplot layout 4,2 scale 1.35,1.35
-  splot './conical-0' u 1:2:($3>dry?$3+$4:1e1000)
-  splot './level-0'
-  splot './conical-1' u 1:2:($3>dry?$3+$4:1e1000)
-  splot './level-1'
-  splot './conical-2' u 1:2:($3>dry?$3+$4:1e1000)
-  splot './level-2'
-  splot './conical-3' u 1:2:($3>dry?$3+$4:1e1000)
-  splot './level-3'
-  unset multiplot
-  ~~~
-  */
 }
+
+/**
+Which gives the following wave (left column) and mesh (right column)
+evolutions. 
+
+~~~gnuplot Evolution of free surface (left) and mesh (right)
+! awk '{ if ($1 == "file:") file = $2; else print $0 > file; }' < out
+ 
+set term @PNG enhanced size 640,1120 font ",8"
+set output 'evolution.png'
+
+unset key
+unset xtics
+unset ytics
+  
+dry=1e-4
+  
+set size ratio -1
+set pm3d
+set pm3d map
+# set contour base
+set palette defined ( 0 0 0 0.5647, 0.125 0 0.05882 1, 0.25 0 0.5647 1, \
+0.375 0.05882 1 0.9333, 0.5 0.5647 1 0.4392, 0.625 1 0.9333 0,	\
+0.75 1 0.4392 0, 0.875 0.9333 0 0, 1 0.498 0 0 )
+  
+set multiplot layout 4,2 scale 1.35,1.35
+splot './conical-0' u 1:2:($3>dry?$3+$4:1e1000)
+splot './level-0'
+splot './conical-1' u 1:2:($3>dry?$3+$4:1e1000)
+splot './level-1'
+splot './conical-2' u 1:2:($3>dry?$3+$4:1e1000)
+splot './level-2'
+splot './conical-3' u 1:2:($3>dry?$3+$4:1e1000)
+splot './level-3'
+unset multiplot
+~~~
+*/
 
 event logfile (i++) {
 
@@ -195,50 +195,48 @@ event logfile (i++) {
   Here we output surface elevation at the various gauges... */
 
   output_gauges (gauges, {eta});
-
-  /**
-  ... and compare it with experimental data both for the Green-Naghdi
-  solver (blue) and the Saint-Venant solver (magenta). The results for
-  the Green-Naghdi solver are very similar to those of [Kazolea et al,
-  2012](/src/references.bib#kazolea2012), Figure 14 and [Lannes and
-  Marche, 2014](/src/references.bib#lannes2014), Figure 20 and
-  significantly better than the results of the Saint-Venant solver
-  (see also e.g. Figure 18 of [Hou et al,
-  2013](/src/references.bib#hou2013) and Figure 19 of [Nikolos and
-  Delis, 2009](/src/references.bib#nikolos2009)) which have too sharp
-  fronts.
-
-  ~~~gnuplot Timeseries of surface elevation. Numerical results and experimental data (symbols).
-  reset
-  set term @PNG enhanced size 640,800
-  set output 'gauges.png'
-  set multiplot layout 5,1 scale 1.,1.
-  set xrange [3:20]
-  t0=22.
-  h0=0.32
-  plot '../ts2b.txt' u ($1-t0):($4-0.001) pt 7 ps 0.25 t '',	\
-  '../conicalsv/WG3' u 1:($2-h0) w l lt 4 t '',			\
-  './WG3' u 1:($2-h0) w l lw 2 lt 3 t 'WG3'
-  plot '../ts2b.txt' u ($1-t0):($6-0.0004) pt 7 ps 0.25 t '',	\
-  '../conicalsv/WG6' u 1:($2-h0) w l lt 4 t '',			\
-  './WG6' u 1:($2-h0) w l lw 2 lt 3 t 'WG6'
-  plot '../ts2b.txt' u ($1-t0):($7) pt 7 ps 0.25 t '',	\
-  '../conicalsv/WG9' u 1:($2-h0) w l lt 4 t '',		\
-  './WG9' u 1:($2-h0) w l lw 2 lt 3 t 'WG9'
-  plot '../ts2b.txt' u ($1-t0):($8-0.0017) pt 7 ps 0.25 t '',	\
-  '../conicalsv/WG16' u 1:($2-h0) w l lt 4 t '',		\
-  './WG16' u 1:($2-h0) w l lw 2 lt 3 t 'WG16'
-  plot '../ts2b.txt' u ($1-t0):($9+0.0015) pt 7 ps 0.25 t '',	\
-  '../conicalsv/WG22' u 1:($2-h0) w l lt 4 t '',		\
-  './WG22' u 1:($2-h0) w l lw 2 lt 3 t 'WG22'
-  unset multiplot
-  
-  ! rm -f conical-?
-  ~~~
-  */
 }
 
 /**
+... and compare it with experimental data both for the Green-Naghdi
+solver (blue) and the Saint-Venant solver (magenta). The results for
+the Green-Naghdi solver are very similar to those of [Kazolea et al,
+2012](/src/references.bib#kazolea2012), Figure 14 and [Lannes and
+Marche, 2014](/src/references.bib#lannes2014), Figure 20 and
+significantly better than the results of the Saint-Venant solver
+(see also e.g. Figure 18 of [Hou et al,
+2013](/src/references.bib#hou2013) and Figure 19 of [Nikolos and
+Delis, 2009](/src/references.bib#nikolos2009)) which have too sharp
+fronts.
+
+~~~gnuplot Timeseries of surface elevation. Numerical results and experimental data (symbols).
+reset
+set term @PNG enhanced size 640,800
+set output 'gauges.png'
+set multiplot layout 5,1 scale 1.,1.
+set xrange [3:20]
+t0=22.
+h0=0.32
+plot '../ts2b.txt' u ($1-t0):($4-0.001) pt 7 ps 0.25 t '',	\
+'../conicalsv/WG3' u 1:($2-h0) w l lt 4 t '',			\
+'./WG3' u 1:($2-h0) w l lw 2 lt 3 t 'WG3'
+plot '../ts2b.txt' u ($1-t0):($6-0.0004) pt 7 ps 0.25 t '',	\
+'../conicalsv/WG6' u 1:($2-h0) w l lt 4 t '',			\
+'./WG6' u 1:($2-h0) w l lw 2 lt 3 t 'WG6'
+plot '../ts2b.txt' u ($1-t0):($7) pt 7 ps 0.25 t '',	\
+'../conicalsv/WG9' u 1:($2-h0) w l lt 4 t '',		\
+'./WG9' u 1:($2-h0) w l lw 2 lt 3 t 'WG9'
+plot '../ts2b.txt' u ($1-t0):($8-0.0017) pt 7 ps 0.25 t '',	\
+'../conicalsv/WG16' u 1:($2-h0) w l lt 4 t '',		\
+'./WG16' u 1:($2-h0) w l lw 2 lt 3 t 'WG16'
+plot '../ts2b.txt' u ($1-t0):($9+0.0015) pt 7 ps 0.25 t '',	\
+'../conicalsv/WG22' u 1:($2-h0) w l lt 4 t '',		\
+'./WG22' u 1:($2-h0) w l lw 2 lt 3 t 'WG22'
+unset multiplot
+  
+! rm -f conical-?
+~~~
+
 We adapt the mesh based on the wavelet-estimated error on the free
 surface elevation. */
 
