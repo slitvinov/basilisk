8d8838104cb7777d7f809d52c465c489f03bc91f
Stephane Popinet <popinet@basilisk.fr>
2015-11-11 11:22:15 -0500

    Added missing plot file for curvature test
    Ignore-this: 218fb79ac5395c0dca81d57d13b997e5
    
    darcs-hash:da55233d5a37f7f58bd7a4e7475e7c431e29d01c

--- src/test/curvature.c
+++ src/test/curvature.c
@@ -157,7 +157,7 @@ set xlabel 'Diameter (grid points)'
 set ylabel 'Relative curvature error / percentage' 
 f(x)=(x > 0. ? 100.*x : 1e1000)
 plot 2./(x*x) t '2/x^{2}', 'log' u 1:4 w lp t 'Max', '' u 1:3 w lp t 'RMS', \
-  'popinet.csv' u ($1*2):2 w lp t 'Popinet (2009)', \
+  '../popinet.csv' u ($1*2):2 w lp t 'Popinet (2009)', \
   'log' u 1:(f($5)) w lp t 'HF', '' u 1:(f($6)) w lp t 'HF fit', \
   '' u 1:(f($7)) w lp t 'Average', '' u 1:(f($8)) w lp t 'Centroids'
 ~~~
