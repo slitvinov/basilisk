5ef926a965a86bc442b15236c6f2fe107f657973
Stephane Popinet <popinet@basilisk.fr>
2017-03-31 02:18:32 -0400

    Momentum-conserving two-phase flow formulation
    Ignore-this: 78a2116b06c34bca33376a8cb010f43b
    
    darcs-hash:3261848c9ebd5d1aed0b6210c12add9cac2773a8

--- src/test/oscillation.c
+++ src/test/oscillation.c
@@ -15,40 +15,106 @@ This simulation is also a stringent test case of the accuracy of the
 surface tension representation as no explicit viscosity can damp
 eventual parasitic currents. 
 
-We use a the Navier--Stokes solver with VOF interface tracking and
-surface tension. */
+We use either the momentum-conserving or standard Navier--Stokes
+solver with VOF interface tracking and surface tension. */
 
-#include "navier-stokes/centered.h"
-#include "vof.h"
-#include "tension.h"
+#if MOMENTUM
+
+# include "momentum.h"
+# include "tension.h"
+# define cf
+
+#else // standard centered Navier--Stokes solver
+
+# include "navier-stokes/centered.h"
+# include "vof.h"
+# include "tension.h"
 
 /**
-The interface is represented by the volume fraction field *c*. */
+The interface is represented by the volume fraction field *f*. */
 
-scalar c[], * interfaces = {c};
+scalar f[], * interfaces = {f};
 
 /**
-The diameter of the droplet is 0.2. The density inside the droplet is
-one and outside 10^-3^. */
+The density inside the droplet is one and outside 10^-3^. */
+
+#define rho(f) (clamp(f,0,1)*(1. - 1e-3) + 1e-3)
+
+/**
+We have the option of using some "smearing" of the density jump. */
+
+#if 0
+#define cf f
+#else
+scalar cf[];
+#endif
+
+/**
+The density is variable. We allocate a new field to store its
+inverse. */
+
+face vector alphav[];
+
+/**
+The density is defined at each timestep via the *properties()* event
+declared by the Navier--Stokes solver. */
+
+event properties (i++) {
+
+  /**
+  When using smearing of the density jump, we initialise *cf* with the
+  vertex-average of *f*. */
+
+#ifndef cf
+  foreach()
+    cf[] = (4.*f[] + 
+	    2.*(f[0,1] + f[0,-1] + f[1,0] + f[-1,0]) +
+	    f[-1,-1] + f[1,-1] + f[1,1] + f[-1,1])/16.;
+  boundary ({cf});
+#endif
+
+  /**
+  The inverse of the density $\alpha$ is then given by the
+  face-averaged value of *cf* and the arithmetic average of density
+  defined by *rho()*. */
+
+  foreach_face() {
+    double cm = (cf[] + cf[-1])/2.;
+    alphav.x[] = 1./rho(cm);
+  }
+  boundary ((scalar *){alphav});  
+}
+
+#endif // standard centered Navier--Stokes solver
+
+/**
+The diameter of the droplet is 0.2. */
 
 #define D 0.2
-#define rho(c) (clamp(c,0,1)*(1. - 1e-3) + 1e-3)
 
 /**
 We will vary the level of refinement to study convergence. */
 
 FILE * fp = NULL;
-
 int LEVEL;
 
 int main() {
+
+  /**
+  The density is variable. */
+
+#if MOMENTUM
+  rho1 = 1, rho2 = 1e-3;
+#else
+  alpha = alphav;
+#endif
   
   /**
   The surface tension is unity. Decreasing the tolerance on the
   Poisson solve improves the results. We cleanup existing files and
   vary the level of refinement. */
 
-  c.sigma = 1.;
+  f.sigma = 1.;
   TOLERANCE = 1e-4;
   remove ("error");
   remove ("laplace");
@@ -73,74 +139,18 @@ int main() {
   system ("grep ^fit out >> log");
 }
 
-/**
-We have the option of using some "smearing" of the density jump. */
-
-#if 0
-#define cf c
-#else
-scalar cf[];
-#endif
-
-/**
-The density is variable. We allocate a new field to store its
-inverse. */
-
-face vector alphav[];
-
 event init (i = 0) {
 
   /**
   We initialise the shape of the interface, a slightly elliptic droplet. */
 
-  fraction (c, D/2.*(1. + 0.05*cos(2.*atan2(y,x))) - sqrt(sq(x) + sq(y)));
-#ifndef cf
-  foreach()
-    cf[] = c[];
-#endif
-
-  /**
-  The density is variable, and defined by the function below. */
-
-  alpha = alphav;
-}
-
-/**
-The density is defined at each timestep via the *properties()* event
-declared by the Navier--Stokes solver. */
-
-event properties (i++) {
-
-  /**
-  When using smearing of the density jump, we initialise *cf* with the
-  vertex-average of *c*. */
+  fraction (f, D/2.*(1. + 0.05*cos(2.*atan2(y,x))) - sqrt(sq(x) + sq(y)));
 
 #ifndef cf
   foreach()
-    cf[] = (4.*c[] + 
-	    2.*(c[0,1] + c[0,-1] + c[1,0] + c[-1,0]) +
-	    c[-1,-1] + c[1,-1] + c[1,1] + c[-1,1])/16.;
-  boundary ({cf});
+    cf[] = f[];
 #endif
-
-  /**
-  The inverse of the density $\alpha$ is then given by the
-  face-averaged value of *cf* and the arithmetic average of density
-  defined by *rho()*. */
-
-  foreach_face() {
-    double cm = (cf[] + cf[-1])/2.;
-    alphav.x[] = 1./rho(cm);
-  }
-  boundary ((scalar *){alphav});  
 }
-
-/**
-By default tracers are defined at $t-\Delta t/2$. We use the *first*
-keyword to move VOF advection before the *logfile* output i.e. at
-$t+\Delta/2$. This improves the results. */
-
-event vof (i++, first);
 
 /**
 At each timestep we output the kinetic energy. */
@@ -148,7 +158,11 @@ At each timestep we output the kinetic energy. */
 event logfile (i++; t <= 1) {
   double ke = 0.;
   foreach (reduction(+:ke))
+#if MOMENTUM
+    ke += sq(Delta)*(sq(q.x[]) + sq(q.y[]))/rho[];
+#else
     ke += sq(Delta)*(sq(u.x[]) + sq(u.y[]))*rho(cf[]);
+#endif
   fprintf (fp, "%g %g %d\n", t, ke, mgp.i);
   fflush (fp);
 }
@@ -209,7 +223,14 @@ event gfsview (i += 10) {
 
 #if TREE
 event adapt (i++) {
-  adapt_wavelet ({c,u}, (double[]){5e-3,1e-3,1e-3}, LEVEL);
+#if MOMENTUM
+  vector u[];
+  foreach()
+    foreach_dimension()
+      u.x[] = q.x[]/rho[];
+  boundary ((scalar *){u});
+#endif
+  adapt_wavelet ({f,u}, (double[]){5e-3,1e-3,1e-3}, LEVEL);
 }
 #endif
 
@@ -217,7 +238,6 @@ event adapt (i++) {
 ## Results
 
 ~~~gnuplot Evolution of the kinetic energy as a function of time for the spatial resolutions (number of grid points per diameter) indicated in the legend. The black lines are fitted decreasing exponential functions.
-set output 'k.png'
 set xlabel 'Time'
 set ylabel 'Kinetic energy'
 set logscale y
@@ -228,18 +248,21 @@ plot [0:1][8e-5:]'k-8' t "51.2" w l, 'k-7' t "25.6" w l,               \
 ~~~
 
 ~~~gnuplot Relative error in the oscillation frequency as a function of resolution.
-set output 'frequency.png'
 set xlabel 'Diameter (grid points)'
 set ylabel 'Frequency error (%)'
 set logscale x 2
 unset grid
 set xzeroaxis
 set key spacing 1.5 top right
-ftitle(a,b) = sprintf("%.0f/x^{%4.2f}", exp(a), -b)
+ftitle(a,b,c) = sprintf("%.0f/x^{%4.2f} (%s)", exp(a), -b, c)
 f(x)=a+b*x
 fit f(x) 'error' u (log($1)):(log(abs($2)*100.)) via a,b
+f1(x)=a1+b1*x
+fit f1(x) '../oscillation-momentum/error' u (log($1)):(log(abs($2)*100.)) via a1,b1
 plot 'error' u ($1):(abs($2)*100.) t "" w p pt 5 ps 2, \
-  exp(f(log(x))) t ftitle(a,b)
+     '../oscillation-momentum/error' u ($1):(abs($2)*100.) t "" w p pt 7 ps 2, \
+      exp(f(log(x))) t ftitle(a,b,"standard"), \
+      exp(f1(log(x))) t ftitle(a1,b1,"momentum")
 ~~~
 
 The amount of numerical damping can be estimated by computing an
@@ -255,11 +278,12 @@ The equivalent Laplace number depends on spatial resolution as
 illustrated below.
 
 ~~~gnuplot Equivalent Laplace number estimated from the numerical damping of kinetic energy.
-set output 'laplace.png'
 set xlabel 'Diameter (grid points)'
 set ylabel 'Equivalent Laplace number'
 set grid
-plot 'laplace' t "" w p pt 5 ps 2
+set key bottom right
+plot 'laplace' t "standard" w p pt 5 ps 2, \
+     '../oscillation-momentum/laplace' t "momentum" w p pt 7 ps 2
 ~~~
 
 ## See also
