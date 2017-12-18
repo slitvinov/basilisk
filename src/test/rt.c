503eae9e5be82d110f3c54a51d6bf4caa47d34eb
Stephane Popinet <popinet@basilisk.fr>
2017-12-18 03:58:28 -0500

    The Rayleigh-Taylor test case uses the momentum-conserving solver
    Ignore-this: 46390b8506ef8d987fc35cf820512129
    
    darcs-hash:6a95542580156d229bd930c998bb4421f1fcb190

--- src/test/rt.c
+++ src/test/rt.c
@@ -1,12 +1,11 @@
-#define JACOBI 1 // fixme: does not converge without this
 #include "navier-stokes/centered.h"
-#include "vof.h"
+#include "two-phase.h"
+#include "navier-stokes/conserving.h"
+#if REDUCED
+# include "reduced.h"
+#endif
 
 #define LEVEL 8
-
-scalar f[], * interfaces = {f};
-face vector alphav[];
-scalar rhov[];
 
 uf.n[left]   = 0;
 p[left] = neumann(0.);
@@ -17,47 +16,35 @@ int main() {
   size (4);
   origin (-2, -2);
   init_grid (1 << LEVEL);
-  // viscosity
-  const face vector muc[] = {0.00313,0.00313};
-  mu = muc;
+  mu1 = mu2 = 0.00313;
+  rho1 = 1.225, rho2 = 0.1694;
+#if REDUCED
+  G.y = -9.81;
+#else
+  const face vector g[] = {0,-9.81};
+  a = g;
+#endif
   DT = 5e-3;
-  TOLERANCE = 1e-4;
+  TOLERANCE = 1e-6;
   run();
 }
 
 event init (t = 0) {
+#if TREE  
   mask (x < -0.5 ? left : x > 0.5 ? right : none);
-  
-  const face vector g[] = {0,-9.81};
-  a = g;
-  alpha = alphav;
-  rho = rhov;
-
+#endif
   fraction (f, 0.05*cos (2.*pi*x) + y);
-}
-
-#define rho(f) ((f)*1.225 + (1. - (f))*0.1694)
-
-event properties (i++) {
-  trash ({alphav,rhov});
-  foreach_face() {
-    double ff = (f[] + f[-1,0])/2.;
-    alphav.x[] = fm.x[]/rho(ff);
-  }
-  foreach()
-    rhov[] = cm[]*rho(f[]);
 }
 
 event logfile (i++) {
   stats s = statsf (f);
   printf ("%g %d %g %g %g %g %d %d %d\n", 
-	  t, i, dt, s.sum - 8., s.min, s.max - 1., mgp.i, mgpf.i, mgu.i);
+	  t, i, dt, s.sum - 2., s.min, s.max - 1., mgp.i, mgpf.i, mgu.i);
   assert (s.min >= -1e-10 && s.max <= 1. + 1e-10);
-  assert (fabs (s.sum - 2.) < 5e-5);
+  assert (fabs (s.sum - 2.) < 3.5e-7);
 }
 
-// event interface (t = {0,0.7,0.8,0.9,1.}) {
-event interface (t = 0.7) {
+event interface (t = {0,0.2,0.4,0.8}) {
   output_facets (f, stderr);
   FILE * fp = fopen ("levels", "w");
   scalar l[];
@@ -76,6 +63,22 @@ event gfsview (i += 10) {
 
 #if TREE
 event adapt (i++) {
+  coord momb = {0};
+  foreach()
+    foreach_dimension()
+      momb.x += dv()*rho(f[])*u.x[];
+  
   adapt_wavelet ({f}, (double[]){5e-3}, LEVEL);
+
+  coord moma = {0};
+  foreach()
+    foreach_dimension()
+      moma.x += dv()*rho(f[])*u.x[];
+
+  /**
+  We check that each component of the momentum is conserved. */
+  
+  foreach_dimension()
+    assert (fabs(momb.x - moma.x) < 1e-10);
 }
 #endif
