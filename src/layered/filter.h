4629c064a8bc033d84ba9496308818cab2b832db
Stephane Popinet <popinet@basilisk.fr>
2019-11-13 09:20:00 -0500

    Filter for grid-scale oscillations of the layered solver
    Ignore-this: 60ea20fa2a5b85c35f420a25ff471a5b
    
    darcs-hash:f807f050d224c62730fa2d1d59fa31a63b6a6628

--- src/layered/filter.h
+++ src/layered/filter.h
@@ -1,3 +1,19 @@
+/**
+# Filter for grid-scale oscillations
+
+Because it is colocated, the [layered solver](hydro.h) can be prone to
+grid-scale oscillations. This noise is usually small but can be
+significant when gradients of bathymetry/wave fields are
+under-resolved.
+
+The filter below can be used to reduce this noise. It is inspired from
+the work of [Engquist et al, 1989](#engquist1989).
+
+The strength of the filter is controlled by the filtering timescale
+`filter`: the smaller the value, the stronger the filter. This can be
+interpreted as the timescale during which a disturbance must not move
+to be seen by the filter. */
+
 double filter = 0.;
 
 event viscous_term (i++)
@@ -10,10 +26,24 @@ event viscous_term (i++)
 	  Hm += h[-1], H += h[], Hp += h[1];
         if (Hm > dry && H > dry && Hp > dry) {
 	  double Dp = eta[1] - eta[], Dm = eta[] - eta[-1];
+
+	  /**
+	  The filter is only applied to extrema in the free-surface
+	  height $\eta$ (first condition) contained within two
+	  inflection points (second condition): this effectively
+	  selects only grid-scale oscillations and avoid filtering
+	  smooth extrema. */
+	  
 	  if (Dp*Dm < 0. && ((eta[2] + eta[] - 2.*eta[1])*
 			     (eta[-1] + eta[1] - 2.*eta[]) < 0. ||
 			     (eta[-1] + eta[1] - 2.*eta[])*
 			     (eta[-2] + eta[] - 2.*eta[-1]) < 0.)) {
+
+	    /**
+	    We then compute the shift in the value of $\eta$ necessary
+	    to smooth the extremum, see Algorithm 2.1 in [Engquist et
+	    al, 1989](#engquist1989). */ 
+	    
 	    double dp, dm;
 	    if (fabs(Dp) > fabs(Dm)) {
 	      dp = fabs(Dp);
@@ -25,6 +55,11 @@ event viscous_term (i++)
 	    }
 	    double d = min(dm, dp/2.);
 	    double a = Dp > 0. ? 1. : -1.;
+
+	    /**
+	    We apply only part of the correction, weighted by the
+	    timescale. */
+	    
 	    eta[] += min(dt/filter, 1.)*a*d;
 	    double Hnew = eta[] - zb[];
 	    if (Hnew > dry) {
@@ -39,9 +74,31 @@ event viscous_term (i++)
 	  }
 	}
       }
-    boundary ({eta});
-    boundary (hl);
+
+    scalar * list = list_copy (hl);
     for (int l = 0; l < nl; l++)
-      boundary (tracers[l]);
+      for (scalar s in tracers[l])
+	list = list_append (list, s);
+    list = list_append (list, eta);
+    boundary (list);
+    free (list);
   }
 }
+
+/**
+## References
+
+~~~bib
+@article{engquist1989,
+  title={Nonlinear filters for efficient shock computation},
+  author={Engquist, Bj{\"o}rn and L{\"o}tstedt, Per and 
+          Sj{\"o}green, Bj{\"o}rn},
+  journal={Mathematics of Computation},
+  volume={52},
+  number={186},
+  pages={509--537},
+  year={1989},
+  url={https://www.ams.org/journals/mcom/1989-52-186/S0025-5718-1989-0955750-9/S0025-5718-1989-0955750-9.pdf}
+}
+~~~
+*/
