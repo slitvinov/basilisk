2bc1ec0b82e771e3a1092689e65c5c0c9bde0636
Stephane Popinet <popinet@basilisk.fr>
2019-11-14 11:52:33 -0500

    Documentation for vertical remapping
    Ignore-this: 181055fb2684fb35146209eb29b6838e
    
    darcs-hash:00eab4f0a9ae2cd9635bc3980e690e738f300426

--- src/layered/remap.h
+++ src/layered/remap.h
@@ -1,16 +1,44 @@
+/**
+# Vertical remapping
+
+This implements a simple vertical remapping to "$\sigma$-coordinates"
+(equally-distributed by default).
+
+We use the [PPR Library](https://github.com/dengwirda/PPR) of Engwirda
+and Kelley to perform the remapping. The default settings are using
+the Parabolic Piecewise Method without limiting. */
+
 #include "ppr/ppr.h"
 
 // int edge_meth = p1e_method, cell_meth = plm_method, cell_lim = null_limit;
 int edge_meth = p3e_method, cell_meth = ppm_method, cell_lim = null_limit;
 // int edge_meth = p5e_method, cell_meth = pqm_method, cell_lim = null_limit;
 
+/**
+The distribution of layers can be controlled using the *beta* array
+which defines the ratio of the thickness of each layer to the total
+depth $H$ (i.e. the relative thickness). By default all layers have
+the same relative thickness. */
+
+double * beta = NULL;
+
+event defaults (i = 0)
+{
+  beta = malloc (nl*sizeof(double));
+  for (int l = 0; l < nl; l++)
+    beta[l] = 1./nl;
+}
+
+/**
+The *vertical_remapping()* function takes a list of layer thicknesses
+and the corresponding array of lists of tracers for each layer and
+performs the remapping (defined by *beta*). */
+
 trace
 void vertical_remapping (scalar * hl, scalar ** tracers)
 {
-  int nvar = list_len(tracers[0]) - 1, ndof = 1, npos = nl + 1;
-  assert (tracers[0][0].i == hl[0].i);
+  int nvar = list_len(tracers[0]), ndof = 1, npos = nl + 1;
   foreach() {
-    // remap to sigma coordinates
     double H = 0.;
     for (scalar h in hl)
       H += h[];
@@ -23,54 +51,48 @@ void vertical_remapping (scalar * hl, scalar ** tracers)
       for (scalar h in hl) {
 	zpos[l+1] = zpos[l] + h[];
 	int i = nvar*l;
-	scalar * list = tracers[l] + 1;
-	for (scalar s in list)
+	for (scalar s in tracers[l])
 	  fdat[i++] = s[];
-#if 1
-	h[] = beta[l]*H;
-#else
-	double z0 = - 0.1;
-	if (l < nl/2)
-	  h[] = (z0 - zb[])/(nl/2);
-	else
-	  h[] = (H + zb[] - z0)/(nl/2);
-#endif
+	h[] = H*beta[l];
 	znew[l+1] = znew[l] + h[];
 	l++;
       }
 
-#if 1      
-      double H1 = 0.;
-      for (scalar h in hl)
-	H1 += h[];
-      assert (fabs(H - H1) < 1e-8);
-#endif
-      
       my_remap (&npos, &npos, &nvar, &ndof, zpos, znew, fdat, fnew,
 		&edge_meth, &cell_meth, &cell_lim);
 
       l = 0;
       for (scalar h in hl) {
 	int i = nvar*l;
-	scalar * list = tracers[l] + 1;
-	for (scalar s in list)
+	for (scalar s in tracers[l])
 	  s[] = fnew[i++];
 	l++;
       }
     }
   }
   
-  scalar * list = NULL;
-  for (int l = 0; l < nl; l++) {
-    scalar * ll = tracers[l];
-    for (scalar s in ll)
+  scalar * list = list_copy (hl);
+  for (int l = 0; l < nl; l++)
+    for (scalar s in tracers[l])
       list = list_append (list, s);
-  }
   boundary (list);
   free (list);
 }
 
+/**
+The remapping is applied at every timestep just before the vertical
+viscosity i.e. just after horizontal advection in the [multilayer
+solver](hydro.h). */
+
 event viscous_term (i++) {
   if (nl > 1)
     vertical_remapping (hl, tracers);
+}
+
+/**
+The *beta* array is freed at the end of the run. */
+
+event cleanup (i = end)
+{
+  free (beta), beta = NULL;
 }
