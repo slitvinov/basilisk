2a398efe99a3835795b3ea090d892072e110f860
Stephane Popinet <popinet@basilisk.fr>
2020-11-13 03:20:55 -0500

    Better progress indicator for multiple runs
    Ignore-this: 14b8963183585e8b2f7a95ed31a2463d
    
    darcs-hash:fb530096f932f85a312dfcfb1cad4b7f962e161f

--- src/grid/progress.h
+++ src/grid/progress.h
@@ -1,6 +1,7 @@
 static int _ilast = -1;
 static double _tlast = -1;
 static timer _progresst;
+static int _progressn = 0;
 
 static void last_events()
 {
@@ -14,14 +15,14 @@ static void last_events()
   else {
     // look for condition on maximum time
     double tmin = 0., tmax = HUGE;
-    while ((tmax - tmin)/(tmax + tmin) > 0.01) {
+    while ((tmax - tmin)/(tmax + tmin) > 0.01 && tmax - tmin > 1e-30) {
       t = (tmin + tmax)/2.;
       if (events (false))
 	tmin = t;
       else
 	tmax = t;
     }
-    if (t < HUGE/2.)
+    if (t < HUGE/2. && t > 1e-30)
       _ilast = 0, _tlast = t;
     else {
       // look for condition on maximum number of iterations
@@ -33,7 +34,6 @@ static void last_events()
     }
   }
   enable_fpe (FE_DIVBYZERO|FE_INVALID);
-  _progresst = timer_start();
 #if 0
   fprintf (stderr, "_tlast: %g _ilast: %d\n", _tlast, _ilast);
   exit (1);
@@ -43,6 +43,10 @@ static void last_events()
 event _progress (i += 5)
 {
   static FILE * fp = fopen ("progress", "w");
+  if (i == 0) {
+    _progresst = timer_start();
+    _progressn++;
+  }
   double peri = _ilast ? i/(double)_ilast : 1.,
     pert = _tlast ? t/_tlast : 1., per = min(peri, pert);
   if (per > 0.01 && per < 1.) {
@@ -50,12 +54,11 @@ event _progress (i += 5)
     double rem = per ? timer_elapsed (_progresst)*(1. - per)/per : 0.;
     if (rem > 0.) {
       double min = floor(rem/60.);
-      fprintf (fp, ", %02.0f:%02.0f remaining\r", min, rem - 60.*min);
+      fprintf (fp, ", %02.0f:%02.0f remaining", min, rem - 60.*min);
     }
-    else
-      fputc ('\r', fp);    
+    if (_progressn > 1)
+      fprintf (fp, " (run #%d)", _progressn);
   }
-  else
-    fputc ('\r', fp);
+  fputc ('\r', fp);    
   fflush (fp);
 }
