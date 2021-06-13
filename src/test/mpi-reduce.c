f8575fcd63194180d4a7dfcd89fe78a17d418a6e
j.a.v.hooft <j.a.v.hooft@gmail.com>
2021-06-13 06:02:28 -0400

    array reductions test
    Ignore-this: 41c6590928a202764ab16d1cfb32d180
    
    darcs-hash:e49a4b6c4b0f47d9514922824d0a7d3f46f19022

--- src/test/mpi-reduce.c
+++ src/test/mpi-reduce.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#define arr_size 10
 
 int main ()
 {
@@ -12,4 +13,13 @@ int main ()
   // statsf() uses MPI reduction operations
   stats stat = statsf (s);
   fprintf (qerr, "%g %g %g\n", stat.min, stat.sum, stat.max);
+
+  // Array reduction
+  int cells[arr_size] = {0};
+  foreach(reduction(+:cells[:arr_size])) 
+    cells[(int)(10*fabs(x))]++;
+
+  for (int i = 0; i < arr_size; i++) 
+    fprintf (qerr, "%d ", cells[i]);
+  fputc ('\n', qerr);
 }
