6ad0553b08b19fefc9c11eeb4499549e4726e7cf
Stephane Popinet <popinet@basilisk.fr>
2021-08-17 05:24:25 -0400

    Various improvements for the soluble gas test case
    Ignore-this: ed7b5fb1d06a824ed8b6e71e0f91028e
    
    darcs-hash:3daea537c5f4c2168dfb32fcf6daf9eff03db45b

--- src/test/static_bubble.c
+++ src/test/static_bubble.c
@@ -2,36 +2,21 @@
 # Soluble gas diffusing from a static bubble
 
 This is the example discussed in section 3.3.1 of [Farsoiya et al.,
-2020](#farsoiya2020).
+2021](#farsoiya2021).
 
-<center>
-<table>
-<tr>
-<td>![](static_bubble/final.png){ width="400px" }</td>
-<td>![](static_bubble/p001cbt.png){ width="400px" }</td>
-<td>![](static_bubble/p001clt.png){ width="400px" }</td>
-</tr>
-<tr>
-<td><center>Diffusion from bubble</center></td> 
-<td><center>Concentration field inside bubble</center></td> 
-<td><center>Concentration field outside bubble</center></td> 
-</tr>
-</table>
-</center>
+The concentration at a point inside and outside the bubble is compared
+with the analytical solution provided in [Farsoiya et al.,
+2021](#farsoiya2021).
 
-The concentration at a point inside and outside the bubble is compared with the analytical solution provided in [Farsoiya et al.,
-2020](#farsoiya2020).
-
-~~~pythonplot
-
+~~~pythonplot Concentration field inside the bubble
 import numpy as np
 import matplotlib.pyplot as plt
 	
 plt.figure()
-t,Gb,Gl = np.loadtxt('../ref_static_bubble',delimiter=' ',unpack=True);
+t,Gb,Gl = np.loadtxt('static_bubble.sol',delimiter=' ',unpack=True);
 plt.plot(t/40, Gb,'k',label='Analytical')
 
-ts,cb9,cl9 = np.loadtxt('out',delimiter=' ',unpack=True)
+ts,cb9,cl9 = np.loadtxt('log',delimiter=' ',unpack=True)
 plt.plot(ts/40,cb9,'k--',label=r'$d_0/\Delta x \approx 102$');
 # plt.ylim(0.97,1.007)
 
@@ -40,8 +25,10 @@ plt.xlabel(r'$t\; \mathscr{D}_l/d_0^2$')
 plt.ylabel(r'$c_b/c_{b0}$')
 plt.tight_layout()
 
-plt.savefig('p001cbt.png')
+plt.savefig('p001cbt.svg')
+~~~
 
+~~~pythonplot Concentration field outside the bubble
 plt.figure()
 
 plt.plot(t/40, Gl,'k',label='Analytical')
@@ -54,92 +41,57 @@ plt.xlabel(r'$t\; \mathscr{D}_l/d_0^2$')
 plt.ylabel(r'$c_l/c_{b0}$')
 plt.tight_layout()
 
-plt.savefig('p001clt.png')
-plt.figure()
-
-plt.savefig(' ')
-
+plt.savefig('p001clt.svg')
 ~~~
 
 ## References
 
 ~~~bib
-@article{farsoiya2020,
-  title = {Bubble mediated gas transfer of diluted component in turbulence},
-  author = {P. K. Farsoiya and S. Popinet and L. Deike},
-  journal = {Journal of Fluid Mechanics},
-  year = {2020},
-  note = {submitted}
-}
+@hal{farsoiya2021, hal-03227997}
 ~~~
 */
 
 #include "axi.h"
 #include "navier-stokes/centered.h"
 #include "two-phase.h"
-#include "navier-stokes/conserving.h"
-#include "tension.h"
-#include "reduced.h"
 #include "henry.h"
-#include "view.h"
 
 scalar c[], * stracers = {c};
 double bubble_radius = 1.;
 double box_size = 10.;
 double conc_liq1 = 0, conc_gas1 = 1.;
-	
-int MAXLEVEL;
+
+int MAXLEVEL = 9;
 
 int main (int argc, char **argv)
 {
   size (box_size);
 	
-  MAXLEVEL = 9;
   N = 1 << MAXLEVEL;
-
 
   rho1 = 1.;
   rho2 = 0.01;
   c.alpha = 0.001;
   TOLERANCE = 1e-4;
-	
  
   c.D1 = 0.1;
   c.D2 = 1.;
-    run();
-  
+  DT = 1.;
+  run();
 }
 
 event init (t = 0)
 {
-
   fraction (f, - (sq(bubble_radius) - sq(x - box_size*0.5) - sq(y)));
-
   foreach()
     c[] = conc_liq1*f[] + conc_gas1*(1. - f[]);
+  boundary ({c});
 }
-
 	
-event extract (t = 0; t += 1.; t <= 48.)
+event extract (i++; t <= 48)
 {		
   if (i == 0)
-    fprintf (stdout, "#t ci co\n");
-  fprintf(stdout,"%e %.12e %.12e\n",t,interpolate(c,5.0,0.5),interpolate(c,5.0,1.5));
-}
-
-event pictures (t = end)
-{
-  char name[80];
-  dump(file="end");
-  view (fov = 9, 
-	tx = -0.5,
-	width = 400, height = 400);  
-  squares ("c", spread = 1, linear = true, map = cool_warm);
-  draw_vof ("f");
-  mirror ({0,1}) {
-    squares ("c", spread = 1, linear = true, map = cool_warm);
-    draw_vof ("f");
-  }
-  sprintf (name, "final.png");
-  save (name);
+    fprintf (stderr, "# t ci co\n");
+  fprintf (stderr, "%e %.12e %.12e\n",
+	   t, interpolate(c,5.0,0.5), interpolate(c,5.0,1.5));
 }
